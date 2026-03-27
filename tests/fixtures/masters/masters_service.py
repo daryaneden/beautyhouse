e040f2f50abdfab5feed793dc5e0126d8cd65449
Dasha <koshka_1994@bk.ru>
2026-03-27 12:06:34 +0300

    fix

--- tests/fixtures/masters/masters_service.py
+++ tests/fixtures/masters/masters_service.py
@@ -1,5 +1,5 @@
 import pytest_asyncio
-from masters.auth.service import MasterAuthService
+from app.masters.auth.service import MasterAuthService
 from app.masters.profile.service import MasterProfileService
 from app.masters.profile.repository import MasterProfileRepository
 from app.settings import Settings 
