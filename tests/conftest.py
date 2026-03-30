dec504c4b6aa64f5c0c3abdfc47007ea30bd312c
Dasha <koshka_1994@bk.ru>
2026-03-30 15:32:05 +0300

    conf

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,6 @@ pytest_plugins = [
     'tests.fixtures.beauty_services.beauty_services_service',
     'tests.fixtures.beauty_services.beauty_services_repository',
     'tests.fixtures.masters.master_profile_repository'
-]
+] 
 
 
