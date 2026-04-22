ad4a0e374b5e03eded80c9a761ec563ebef214f5
Dasha <koshka_1994@bk.ru>
2026-04-22 18:43:47 +0300

    22apr

--- tests/fixtures/beauty_services/beauty_services_use_cases.py
+++ tests/fixtures/beauty_services/beauty_services_use_cases.py
@@ -5,5 +5,5 @@ from app.application.beauty_services.use_cases import BeautyServicesUsecases
 
 @pytest_asyncio.fixture()
 async def beauty_services_use_cases(get_test_session):
-    return BeautyServicesUsecases(beauty_service_repository=SQLAlchemyBeautyServicesRepository(db_session=get_test_session))
+    return BeautyServicesUsecases(repo=SQLAlchemyBeautyServicesRepository(db_session=get_test_session))
 
--- tests/fixtures/masters/masters_use_cases.py
+++ tests/fixtures/masters/masters_use_cases.py
@@ -2,16 +2,16 @@ import pytest_asyncio
 from app.application.masters.auth.use_cases import MasterAuthUseCases
 from app.application.masters.profile.use_cases import MasterProfileUseCases
 from app.infrastructure.masters.repository import SQLAlchemyMastersRepository
+from app.infrastructure.jwt_service import JwtService
 from app.setting import Settings 
 
 @pytest_asyncio.fixture()
 async def master_auth_use_cases(get_test_session):
-    return MasterAuthUseCases(master_profile_repository=SQLAlchemyMastersRepository(db_session=get_test_session),
-                             settings=Settings())
+    return MasterAuthUseCases(repo=SQLAlchemyMastersRepository(db_session=get_test_session),
+                             jwt_provider=JwtService())
 
 @pytest_asyncio.fixture(scope='session')
 async def master_profile_use_cases(get_test_session):
-    return MasterProfileUseCases(master_profile_repository=SQLAlchemyMastersRepository(db_session=get_test_session),
-                                master_auth_service=MasterAuthUseCases(master_profile_repository=SQLAlchemyMastersRepository(db_session=get_test_session), 
-                                                                       settings=Settings()  
-                                ))
+    return MasterProfileUseCases(repo=SQLAlchemyMastersRepository(db_session=get_test_session),
+                                jwt_provider=JwtService()  
+                                )
